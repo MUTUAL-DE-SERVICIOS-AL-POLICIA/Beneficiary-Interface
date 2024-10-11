1db732de9e790ae124de6badd8b34b6c14133d2a
Adrian-Herrera-96 <adrirha@gmail.com>
2024-10-11 18:37:14 -0400

    Se añadio comprobacion de token y archivo prettier

--- app/beneficiaries/service.ts
+++ app/beneficiaries/service.ts
@@ -1,32 +1,38 @@
-"use server"
-import { apiClient } from "@/services"
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+'use server';
+import { apiClient } from '@/services';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
-// export const getCookies = async () => {
-//    console.log("getCookies");
+export const getCookies = async () => {
+  console.log('getCookies');
 
-//    const cookieStore = cookies();
-//    const cookie = cookieStore.get("Set-Cookie");
+  const cookieStore = cookies();
+  const cookie = cookieStore.get('Set-Cookie');
 
-//    return cookie;
-// };
+  return cookie;
+};
 
-export const getBeneficiaries = async (limit: number = 10, page: number = 1, filter?:string ): Promise<any> => {
-   try {
-      // const cookie = await getCookies();
-      // console.log(cookie)
-      // if (cookie == undefined) {
-      //    console.log("Sin cookie");
-      //    redirect('http://192.168.2.201:3000/');
-      // }
-      const beneficiaries = await apiClient.GET('api/persons', {
-         limit,
-         page,
-         ...(filter ? { filter } : {})
-      })
-      return beneficiaries
-   } catch(e:any) {
-      throw e
-   }
-}
+export const getBeneficiaries = async (
+  limit: number = 10,
+  page: number = 1,
+  filter?: string,
+): Promise<any> => {
+  const host = process.env.NEXT_PUBLIC_BACKEND_HOST;
+  const port = process.env.LOGIN_FRONTEND_PORT;
+  try {
+    const cookie = await getCookies();
+    console.log(cookie);
+    if (cookie == undefined) {
+      console.log('Sin cookie');
+      redirect(`http://${host}:${port}/`);
+    }
+    const beneficiaries = await apiClient.GET('api/persons', {
+      limit,
+      page,
+      ...(filter ? { filter } : {}),
+    });
+    return beneficiaries;
+  } catch (e: any) {
+    throw e;
+  }
+};
