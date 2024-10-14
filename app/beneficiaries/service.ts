96496a809a4be53370ed5a43075b6d18b2e85cfa
Adrian-Herrera-96 <adrirha@gmail.com>
2024-10-14 16:50:59 -0400

    Actualización de env example

--- app/beneficiaries/service.ts
+++ app/beneficiaries/service.ts
@@ -17,7 +17,7 @@ export const getBeneficiaries = async (
   page: number = 1,
   filter?: string,
 ): Promise<any> => {
-  const host = process.env.NEXT_PUBLIC_BACKEND_HOST;
+  const host = process.env.NEXT_PUBLIC_SERVER_FRONTEND;
   const port = process.env.LOGIN_FRONTEND_PORT;
   try {
     const cookie = await getCookies();
