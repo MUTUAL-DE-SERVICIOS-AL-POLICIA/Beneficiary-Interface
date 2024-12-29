179c5224432b8b596afdc302b1445110a689c701
MaxRamirez <leorexx100@gmail.com>
2024-12-29 12:25:53 -0400

    Integrando api para la creación de documentos, Diseño de la interfaz de usuario para la creación de nuevos documentos

--- app/beneficiary/[id]/(profile)/affiliate-data/page.tsx
+++ app/beneficiary/[id]/(profile)/affiliate-data/page.tsx
@@ -5,17 +5,53 @@ import { EntryInfo } from './(sections)/EntryInfo';
 import { StateInfo } from './(sections)/StateInfo';
 import { ServiceInfo } from './(sections)/ServiceInfo';
 import { DerelictInfo } from './(sections)/DerelictInfo';
-import { getAffiliate, obtainAffiliateDocuments } from '@/app/beneficiary/service';
+import { getAffiliate, getAllDocuments, obtainAffiliateDocuments } from '@/app/beneficiary/service';
 import { useBeneficiary } from '@/context/BeneficiaryContext';
 import { AffiliateDocuments } from './(sections)/Documents';
 import { useAlert } from '@/hooks/useAlerts';
+import { Button } from '@nextui-org/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolderPlus, faRegistered } from '@fortawesome/free-solid-svg-icons';
+import { Tooltip } from '@nextui-org/tooltip';
+import ModalRegistrationComponent from '@/components/modal-registration';
+import { apiClient } from '@/services';
 
 export default function AffiliateDataPage() {
   const [affiliate, setAffiliate] = useState<any>({});
   const [documents, setDocuments] = useState<any>([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [allDocuments, setAllDocuments] = useState<any>([]);
   const { beneficiaryData, error } = useBeneficiary();
 
   const { Alert } = useAlert();
+  const toggleDialog = () => setIsDialogOpen(!isDialogOpen);
+
+  const handleDocumentRecord = async () => {
+    const response = await getAllDocuments();
+    setAllDocuments(response.data);
+    toggleDialog();
+  };
+
+  const registerFile = async (file: any, selectedKey: any) => {
+    const formData = new FormData();
+    formData.append('documentPdf', file);
+    try {
+      const response = await apiClient.POST(
+        `/api/affiliates/${affiliate.id}/document/${selectedKey}/createOrUpdate`,
+        formData,
+        true,
+      );
+      console.log(response);
+      const affiliateId = beneficiaryData.personAffiliate[0].typeId;
+      const res = await obtainAffiliateDocuments(affiliateId);
+      setDocuments(res.data);
+    } catch (e: any) {
+      console.log(e);
+      console.error('Error al cargar el archivo');
+    } finally {
+      toggleDialog();
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -68,8 +104,13 @@ export default function AffiliateDataPage() {
       <div className="px-3 py-1">
         <DerelictInfo affiliate={affiliate} />
       </div>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mr-3">
         <h1 className="text-md uppercase font-semibold">Documentos presentados</h1>
+        <Tooltip content="Registrar nuevo documento">
+          <Button isIconOnly onPress={handleDocumentRecord}>
+            <FontAwesomeIcon icon={faFolderPlus} size="xl" />
+          </Button>
+        </Tooltip>
       </div>
       <Divider className="bg-gray-400 mb-5 w-full" />
       <div className="px-3 py-1">
@@ -79,6 +120,12 @@ export default function AffiliateDataPage() {
           </div>
         </div>
       </div>
+      <ModalRegistrationComponent
+        open={isDialogOpen}
+        onOpenChange={toggleDialog}
+        data={allDocuments}
+        action={registerFile}
+      />
     </div>
   );
 }
