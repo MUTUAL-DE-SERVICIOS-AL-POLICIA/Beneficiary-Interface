cdeb21085e83b39e31754b63ca83e176b972daed
MaxRamirez <leorexx100@gmail.com>
2025-01-03 10:40:02 -0400

    Agregando breadcrumbs a la interfaz

---
+++ components/breadcrumbs.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/breadcrumbs";
+import Link from "next/link";
+
+export default function BreadcrumbsComponent() {
+  return (
+    <div className="flex justify-end mr-8 mb-2">
+      <Breadcrumbs radius="sm" variant="light">
+        <BreadcrumbItem>inicio</BreadcrumbItem>
+        <BreadcrumbItem>
+          <Link href="/beneficiaries">Beneficiarios</Link>
+        </BreadcrumbItem>
+      </Breadcrumbs>
+    </div>
+  );
+}
