b0929bf2196c2814259475746b9cc4ab523e8774
ferbautistamollo <ferbautistamollo@gmail.com>
2025-04-03 10:24:33 -0400

    se edita el navbar con el logo muserpol y opciones de usuario

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,9 +11,8 @@ export const Navbar = () => {
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
+          <NextLink className="flex justify-start items-center gap-1" href="/apphub">
             <Logo />
-            <p className="font-bold text-inherit">MUSERPOL</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
