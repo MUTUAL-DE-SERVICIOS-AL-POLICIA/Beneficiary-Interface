6b4c92f5166394cc64088531c8d3a2d1156f2f17
José <jcuencacoronado@gmail.com>
2025-04-28 15:27:36 -0400

    Se agrego lectura de usuario desde la cookie en el navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,30 +2,31 @@ import { NavbarBrand, NavbarContent, NavbarItem, Navbar as NextUINavbar } from "
 import NextLink from "next/link";
 
 import { UserComponent } from "./user";
-
 import { Logo } from "@/components/icons";
 import { ThemeSwitch } from "@/components/theme-switch";
-
-export const Navbar = () => {
+import { User as UserInterface } from "@/utils/interfaces/index";
+import { getUserCookie } from "@/utils/helpers/cookies";
+import { Suspense } from "react";
+export const Navbar = async () => {
+  const { data } = await getUserCookie();
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/apphub">
-            <Logo 
-              height={30}
-              width={80}
-            />
-          </NextLink>
-        </NavbarBrand>
-      </NavbarContent>
+    <Suspense>
+      <NextUINavbar maxWidth="xl" position="sticky">
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+          <NavbarBrand as="li" className="gap-3 max-w-fit">
+            <NextLink className="flex justify-start items-center gap-1" href="/apphub">
+              <Logo height={30} width={80} />
+            </NextLink>
+          </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-        <UserComponent />
-      </NavbarContent>
-    </NextUINavbar>
+        <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
+          <NavbarItem className="hidden sm:flex gap-2">
+            <ThemeSwitch />
+          </NavbarItem>
+          <UserComponent user={data} />
+        </NavbarContent>
+      </NextUINavbar>
+    </Suspense>
   );
 };
