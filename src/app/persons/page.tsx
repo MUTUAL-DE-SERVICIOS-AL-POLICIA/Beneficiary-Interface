528750711fa3c54b6df08a33f6e07a6b53ebfe0d
José <jcuencacoronado@gmail.com>
2025-04-30 18:06:34 -0400

    Se agrego subtitulo

--- src/app/persons/page.tsx
+++ src/app/persons/page.tsx
@@ -3,19 +3,24 @@ import { getPersons } from "@/api/person/api";
 import { TableComponent } from "@/components/persons";
 import { personTableHeaders as headerColumns } from "@/utils/static";
 import { usePersons } from "@/utils/hooks/usePersons";
+import { subtitle } from "../../components/common";
+import { Fragment } from "react";
 
 export default function Persons() {
   const { error, personsData, total } = usePersons();
 
   return (
-    <TableComponent
-      data={personsData}
-      error={error}
-      getData={getPersons}
-      headerColumns={headerColumns}
-      startPage={1}
-      startRowsPerPage={10}
-      total={total}
-    />
+    <Fragment>
+      <span className={subtitle({ fullWidth: true })}>Personas registradas</span>
+      <TableComponent
+        data={personsData}
+        error={error}
+        getData={getPersons}
+        headerColumns={headerColumns}
+        startPage={1}
+        startRowsPerPage={10}
+        total={total}
+      />
+    </Fragment>
   );
 }
