e7fcdf35e5af93eee5356ae54f11e0139390cc91
ferbautistamollo <ferbautistamollo@gmail.com>
2025-05-30 15:17:06 -0400

    IMPORTACIÓN DE ICONOS ATRAVEZ DE INDEX

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,28 @@
-export { BreadcrumbsComponent } from "./breadcrumbs";
-export { MuserpolLogo, Logo, SearchIcon, MoonFilledIcon, SunFilledIcon, VerticalDotsIcon } from "./icons";
+export { BreadcrumbsState } from "./breadcrumbsState";
+export {
+  MuserpolLogo,
+  Logo,
+  SearchIcon,
+  MoonFilledIcon,
+  SunFilledIcon,
+  VerticalDotsIcon,
+  PersonInfoIcon,
+  TouchIcon,
+  ChevronRightIcon,
+  PersonalDataIcon,
+  PoliceDataIcon,
+  DocumentsDataIcon,
+  AffiliateDataIcon,
+  BeneficiariesDataIcon,
+  TouchIndiceIcon,
+  TouchPulgarIcon,
+  CircleIcon,
+  TouchRegisterIcon,
+  DocumentRegisterIcon,
+  DocumentViewIcon,
+  DocumentEditIcon,
+} from "./icons";
 export { InputCustom } from "./input";
 export { title, subtitle } from "./primitives";
 export { ThemeSwitch } from "./theme-switch";
+export { AlertServer } from "./alertServer";
