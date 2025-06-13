b0f23749c253db63d5360e8ce1220b1a7e24e531
José <jcuencacoronado@gmail.com>
2025-06-13 15:15:05 -0400

    Se agrega dateinput y copybutton a archivo barril

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -23,6 +23,8 @@ export {
   DocumentEditIcon,
 } from "./icons";
 export { InputCustom } from "./input";
+export { DateInputCustom } from "./dateInput"
+export { CopyButton } from "./copyButton";
 export { title, subtitle } from "./primitives";
 export { ThemeSwitch } from "./theme-switch";
 export { AlertServer } from "./alertServer";
