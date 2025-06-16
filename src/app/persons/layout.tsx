90aacd517129084d341fea6668af532ad7c49043
ferbautistamollo <ferbautistamollo@gmail.com>
2025-06-16 13:35:41 -0400

    AJUSTE DE CLASE RESPONSIVE TAMAÑO PERSONALIZADOS DIF, TIPOS DE PANTALLLA

--- src/app/persons/layout.tsx
+++ src/app/persons/layout.tsx
@@ -1,5 +1,9 @@
 export const dynamic = "force-dynamic";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  return <section className="flex flex-col justify-center gap-1">{children}</section>;
+  return (
+    <section className="flex flex-col justify-center md:flex-row flex-wrap gap-1 ml-2 mr-2 min-h-[530px] 2xl:min-h-[810px]">
+      {children}
+    </section>
+  );
 }
