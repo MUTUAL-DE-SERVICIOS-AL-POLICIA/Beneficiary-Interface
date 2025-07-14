c3d6dce050fa5909cec45c4ab9159fb6a954ce7f
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-14 11:23:01 -0400

    ARREGLANDO ARCHIVO DE TURRIL DE DATOS DEL POLICIA

--- src/components/persons/sidebarMenu/police/information/index.ts
+++ src/components/persons/sidebarMenu/police/information/index.ts
@@ -1,9 +1,6 @@
 export { Derelict } from "./derelict";
-export { Service } from "./service";
-export { State } from "./state";
 export { Document } from "./document";
 export { Entry } from "./entry";
 export { FileDossier } from "./fileDossier";
-export { ModalDocument } from "./modalDocument";
-export { ModalFileDossier } from "./modalFileDossier";
-export { ViewerPdf } from "./viewerPdf";
+export { Service } from "./service";
+export { State } from "./state";
