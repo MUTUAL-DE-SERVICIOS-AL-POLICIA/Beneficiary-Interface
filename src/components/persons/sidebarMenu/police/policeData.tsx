4079c46080c07b14a2c3e388d10de992bba7a77f
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-03 14:38:06 -0400

    CAMBIOS DE NOMBRE DE RUTAS, UNIFICACIÓN RUTAS BENEFICIARIES

--- src/components/persons/sidebarMenu/police/policeData.tsx
+++ src/components/persons/sidebarMenu/police/policeData.tsx
@@ -41,16 +41,16 @@ export const PoliceData = () => {
           registration={policeData?.registration ?? "Sin registro"}
         />
         <State
-          nameAffiliateState={policeData?.affiliateState.name ?? "Sin registro"}
+          nameAffiliateState={policeData?.affiliateState?.name ?? "Sin registro"}
           type={policeData?.type ?? "Sin registro"}
         />
       </div>
       <Service
         nameCategory={
-          policeData?.category.serviceStatus ? (policeData?.category.name ?? "Sin registro") : "s/n"
+          policeData?.category?.serviceStatus ? (policeData?.category.name ?? "Sin registro") : "s/n"
         }
-        nameDegree={policeData?.degree.serviceStatus ? (policeData?.degree.name ?? "Sin registro") : "s/n"}
-        nameUnit={policeData?.unit.serviceStatus ? (policeData?.unit.name ?? "Sin registro") : "s/n"}
+        nameDegree={policeData?.degree?.serviceStatus ? (policeData?.degree.name ?? "Sin registro") : "s/n"}
+        nameUnit={policeData?.unit?.serviceStatus ? (policeData?.unit.name ?? "Sin registro") : "s/n"}
         serviceMonths={policeData?.serviceMonths ?? "Sin registro"}
         serviceYears={policeData?.serviceYears ?? "Sin registro"}
       />
